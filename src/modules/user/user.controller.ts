af82171ee14344b70820999a2c46d46f2772b6dc
Bertrand-mg <bertrandmutabazig@gmail.com>
2026-01-29 21:54:05 +0200

    Me profile

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -21,37 +21,49 @@ import { RoleGuard } from 'src/common/guards/role.guard';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { userRoleEnum } from 'src/common/enums/userRole.enum';
 import { Roles } from 'src/common/decorator/roles.decorator';
+import { User } from 'src/common/decorator/user.decorator';
+import type { Payload } from 'src/util/token.service';
 
 @Controller('user')
 @UseGuards(AuthGuard)
+@UseGuards(RoleGuard)
 @ApiBearerAuth()
-@Roles(userRoleEnum.ADMIN)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  //me
+
+  @Get('me')
+  @ApiOperation({ summary: 'Get User Profile' })
+  async getMe(@User() user: Payload) {
+    return await this.userService.findOne({ userId: user.sub });
+  }
+
+  //admin
   @Get()
-  @UseGuards(RoleGuard)
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'List Users' })
   async find() {
     return await this.userService.find();
   }
 
   @Get('/:id')
-  @UseGuards(RoleGuard)
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'Find User By Id' })
   async findById(@Param() param: UserIdDTO) {
-    return await this.userService.findOne({ userId: param.userId });
+    const data = await this.userService.findOne({ userId: param.userId });
+    return { data };
   }
 
   @Get('/search')
-  @UseGuards(RoleGuard)
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'Search User' })
   async searchUser(@Query() queries: UserDTO) {
     return await this.userService.find(queries);
   }
 
   @Post()
-  @UseGuards(RoleGuard)
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'Create Admin' })
   async createAdmin(@Body() dto: CreateAdminDTO) {
     await this.userService.create(dto);
@@ -66,6 +78,7 @@ export class UserController {
   // }
 
   @Delete('/delete/:userId')
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'Delete Admin' })
   async deleteUser(@Param() params: UserIdDTO) {
     await this.userService.softDelete(params);
@@ -73,7 +86,7 @@ export class UserController {
   }
 
   @Delete('/remove/:userId')
-  @UseGuards(RoleGuard)
+  @Roles(userRoleEnum.ADMIN)
   @ApiOperation({ summary: 'Remove Admin' })
   async removeUser(@Param() params: UserIdDTO) {
     await this.userService.hardDelete(params);
