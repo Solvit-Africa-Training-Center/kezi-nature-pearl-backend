796ce0bf4738e939afbbbdd609b3e64ef0a538c0
Bertrand-mg <bertrandmutabazig@gmail.com>
2026-02-01 15:05:04 +0200

    Profile upload

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -47,7 +47,7 @@ export class UserController {
   async getMe(@User() logedUser: Payload) {
     const user = await this.userService.findOne(
       { userId: logedUser.sub },
-      { relations: ['file'] },
+      { relations: ['files'] },
     );
 
     if (!user) throw new NotFoundException('User not found');
